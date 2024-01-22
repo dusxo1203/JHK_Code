40aca313abd1b80200e3b718a64427b35572726a
dusxo1203 <dusxo1203@gmail.com>
2024-01-22 11:58:51 +0900

    ch lang

--- JHK_Cpp_Prac/chap1/1장연습문제.cpp
+++ JHK_Cpp_Prac/chap1/1장연습문제.cpp
@@ -4,10 +4,10 @@ void str_prn(char* p, char c = '\0');
 int main()
 {
 	char str[80];
-	cout << "문장 입력 : ";
+	cout << "문장을 입력 : ";
 	cin.getline(str, sizeof(str));
 	char ch;
-	cout << "문자 입력 : ";
+	cout << "문자를 입력 : ";
 	cin >> ch;
 	str_prn(str);
 	str_prn(str, ch);
